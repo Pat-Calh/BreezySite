26c316d043549cd4ff207040513a5e0c76ce84ca
PCalh <pat.calhoun97@gmail.com>
2025-06-30 17:33:37 -0500

    Updated Site Layout and Added new information + Decals

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h2>About Breezy</h2>
+      <p>Breezy is a playful fox fursona who loves coding and music!</p>
+    </div>
+  );
+}
---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h2>Contact Breezy</h2>
+      <p>You can reach Breezy via Twitter, SoundCloud, or Telegram using the links above!</p>
+    </div>
+  );
+}
